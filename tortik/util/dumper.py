f5045e85c7c68013b4956afac07d978210a31d36
Vitaly Glibin <glibin.v@gmail.com>
2014-06-23 10:53:45 +0400

    move debug page to jinja2 templating and dumper refactor

--- tortik/util/dumper.py
+++ tortik/util/dumper.py
@@ -1,57 +1,38 @@
 # -*- coding: utf-8 -*-
 
-import json
 
+def dump(obj):
+    refs = set()
 
-class Dumper():
-    def __init__(self):
-        self._refs = set()
+    def primitive(value, ptype='string'):
+        return dict(type=ptype, value=value)
 
-    @classmethod
-    def dump(cls, obj):
-        return cls().make_dump(obj)
+    def _make_dump(item):
+        if isinstance(item, list):
+            if len(item) == 0:
+                return dict(type='array', value=[])
+            if id(item) in refs:
+                return primitive('<circular reference>')
 
-    def make_dump(self, obj):
-        if isinstance(obj, list):
-            if len(obj) == 0:
-                return '[ ]'
-            if id(obj) in self._refs:
-                return self._dec_with_span(self._js_string('<circular reference>'), 'string')
-            self._refs.add(id(obj))
+            refs.add(id(item))
+            return dict(type='array', value=map(lambda x: _make_dump(x), item))
+        elif isinstance(item, dict):
+            if not item:
+                return dict(type='dict', value=dict())
+            if id(item) in refs:
+                return primitive('<circular reference>')
+            refs.add(id(item))
 
-            return '<span class="dumper__collapser"></span>[<ul class="dumper-collapsible"><li class="dumper-item">' \
-                   + ',</li><li class="dumper-item">'.join(map(lambda x: self.make_dump(x), obj)) + '</li></ul>]'
-        elif isinstance(obj, dict):
-            if not obj:
-                return '{ }'
-            if id(obj) in self._refs:
-                return self._dec_with_span(self._js_string('<circular reference>'), 'string')
-            self._refs.add(id(obj))
-
-            return '<span class="dumper__collapser"></span>{<ul class="dumper-collapsible"><li class="dumper-item">' \
-                   + ',</li><li class="dumper-item">'.join(map(lambda (x, y):
-                                                               '<span class="dumper-item-prop">'
-                                                               + '<span class="dumper-item-prop__q">"</span>'
-                                                               + str(x)
-                                                               + '<span class="dumper-item-prop__q">"</span></span>: '
-                                                               + self.make_dump(y), obj.items())) + \
-                   '</li></ul>}'
-        elif isinstance(obj, bool):
-            return self._dec_with_span('true' if obj else 'false', 'bool')
-        elif isinstance(obj, basestring):
-            return self._dec_with_span(self._js_string(obj), 'string')
-        elif isinstance(obj, (int, float)):
-            return self._dec_with_span(str(obj), 'num')
-        elif obj is None:
-            return self._dec_with_span('null', 'null')
+            return dict(type='dict', value=dict((x, _make_dump(y)) for x, y in item.items()))
+        elif isinstance(item, bool):
+            return primitive('true' if item else 'false', 'bool')
+        elif isinstance(item, basestring):
+            return primitive(item)
+        elif isinstance(item, (int, float)):
+            return primitive(item, 'number')
+        elif item is None:
+            return primitive('null', 'null')
         else:
-            return self._dec_with_span(self._js_string(repr(obj)), 'string')
+            return primitive(repr(item), 'string')
 
-    def _dec_with_span(self, val, cls):
-        return '<span class="dumper-item__' + cls + '">' + val + '</span>'
-
-    def _html_encode(self, val):
-        return val.replace('&', '&amp;').replace('"', '&quot;').replace('<', '&lt;').replace('>', '&gt;')
-
-    def _js_string(self, val):
-        return '"' + self._html_encode(json.dumps(val, ensure_ascii=False)[1:-1]) + '"'
+    return _make_dump(obj)
--- tortik_tests/dumper_test.py
+++ tortik_tests/dumper_test.py
@@ -2,20 +2,15 @@
 
 import unittest
 
-from tortik.util.dumper import Dumper
+from tortik.util.dumper import dump
 
 
 class DumperTestCase(unittest.TestCase):
     def test_circular_reference(self):
         a = {'a': 123}
         a['b'] = a
-        expected = ('<span class="dumper__collapser"></span>{<ul class="dumper-collapsible"><li class="dumper-item">'
-                    '<span class="dumper-item-prop">'
-                    '<span class="dumper-item-prop__q">"</span>a<span class="dumper-item-prop__q">"</span>'
-                    '</span>: <span class="dumper-item__num">123</span>,</li>'
-                    '<li class="dumper-item">'
-                    '<span class="dumper-item-prop">'
-                    '<span class="dumper-item-prop__q">"</span>b<span class="dumper-item-prop__q">"</span>'
-                    '</span>: '
-                    '<span class="dumper-item__string">"&lt;circular reference&gt;"</span></li></ul>}')
-        self.assertEqual(expected, Dumper.dump(a))
+        expected = {'type': 'dict', 'value': {
+            'a': {'type': 'number', 'value': 123},
+            'b': {'type': 'string', 'value': '<circular reference>'}
+        }}
+        self.assertEqual(expected, dump(a))
