0969ac353b75cc36c65dde0b7f70ea2a5f73d5dd
artembelikov <art_corp@list.ru>
2025-03-06 21:30:47 +0400

    QP-12 import registry

--- tortik/metrics.py
+++ tortik/metrics.py
@@ -1,17 +1,48 @@
-from prometheus_client import Counter, Gauge, Histogram, Summary
+from prometheus_client import Counter, Histogram
 
-REQUEST_COUNT = Counter(
-    'tortik_request_count',
-    'Counter of requests processed by the tortik library',
-    ['method', 'endpoint']
-)
+REGISTRY = None
 
-PROCESSING_TIME = Histogram(
-    'tortik_request_processing_seconds',
-    'Histogram of processing time (seconds) for tortik requests',
-    ['operation'],
-    buckets=[0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, 15.0, 20.0, 30.0]
-)
+def init_metrics(registry):
+    global REGISTRY, REQUEST_COUNT, PROCESSING_TIME
+
+    REGISTRY = registry
+
+    REQUEST_COUNT = Counter(
+        'tortik_request_count',
+        'Counter of requests processed by the tortik library',
+        ['method', 'endpoint'],
+        registry = REGISTRY
+    )
+
+    PROCESSING_TIME = Histogram(
+        'tortik_request_processing_seconds',
+        'Histogram of processing time (seconds) for tortik requests',
+        ['method', 'endpoint', 'code'],
+        buckets=[0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, 15.0, 20.0, 30.0],
+        registry=REGISTRY
+    )
+
+class _DummyMetric:
+    def __init__(self):
+        pass
+
+    def labels(self, **kwargs):
+        return self
+
+    def inc(self, amount=1):
+        pass
+
+    def dec(self, amount=1):
+        pass
+
+    def set(self, value):
+        pass
+
+    def observe(self, value):
+        pass
+
+REQUEST_COUNT = _DummyMetric()
+PROCESSING_TIME = _DummyMetric()
 
 
 def count_request(method, endpoint):
