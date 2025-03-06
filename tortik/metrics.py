cd20ba3290b0d1c4d428c090612b1e49f9d5a01e
artembelikov <art_corp@list.ru>
2025-03-06 23:22:32 +0400

    QP-12 added additional logging

--- tortik/metrics.py
+++ tortik/metrics.py
@@ -1,32 +1,12 @@
 from prometheus_client import Counter, Histogram
 
-REGISTRY = None
-
-def init_metrics(registry):
-    global REGISTRY, REQUEST_COUNT, PROCESSING_TIME
-
-    REGISTRY = registry
-
-    REQUEST_COUNT = Counter(
-        'tortik_request_count',
-        'Counter of requests processed by the tortik library',
-        ['method', 'endpoint'],
-        registry = REGISTRY
-    )
-
-    PROCESSING_TIME = Histogram(
-        'tortik_request_processing_seconds',
-        'Histogram of processing time (seconds) for tortik requests',
-        ['method', 'endpoint', 'code'],
-        buckets=[0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, 15.0, 20.0, 30.0],
-        registry=REGISTRY
-    )
-
 class _DummyMetric:
     def __init__(self):
         pass
 
     def labels(self, **kwargs):
+        import logging
+        logging.warning("Uses dummy metric !!!!")
         return self
 
     def inc(self, amount=1):
@@ -44,6 +24,23 @@ class _DummyMetric:
 REQUEST_COUNT = _DummyMetric()
 PROCESSING_TIME = _DummyMetric()
 
+def init_metrics(registry):
+    global REQUEST_COUNT, PROCESSING_TIME
+
+    REQUEST_COUNT = Counter(
+        'tortik_request_count',
+        'Counter of requests processed by the tortik library',
+        ['method', 'endpoint'],
+        registry = registry
+    )
+
+    PROCESSING_TIME = Histogram(
+        'tortik_request_processing_seconds',
+        'Histogram of processing time (seconds) for tortik requests',
+        ['method', 'endpoint', 'code'],
+        buckets=[0.1, 0.25, 0.5, 0.75, 1.0, 2.5, 5.0, 7.5, 10.0, 15.0, 20.0, 30.0],
+        registry=registry
+    )
 
 def count_request(method, endpoint):
     REQUEST_COUNT.labels(method=method, endpoint=endpoint).inc()
